0354f0d31009ed9e5721ca11b5eb5e88c93c0a71
AndSim <siminiucandrew@gmail.com>
2024-02-04 16:31:33 +0200

    feat: load and data questions from `json-server`, `www.github.com/typicode/json-server` and (questions.json)

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,43 @@
 import './App.css';
 import Header from './Header.jsx';
 import AppMain from './AppMain.jsx';
+import { useEffect, useReducer } from 'react';
+
+const initialState = {
+	questions: [],
+
+	status: 'loading',
+};
+
+function reducer(state, action) {
+	switch (action.type) {
+		case 'dataReceived':
+			return {
+				...state,
+				questions: action.payload,
+				status: 'ready',
+			};
+		case 'dataFailed':
+			return {
+				...state,
+				status: 'error',
+			};
+
+		default:
+			throw new Error('Action unknown!');
+	}
+}
 
 export default function App() {
+	const [state, dispatch] = useReducer(reducer, initialState);
+
+	useEffect(function () {
+		fetch('http://localhost:8000/questions')
+			.then((res) => res.json())
+			.then((data) => dispatch({ type: 'dataReceived', payload: data }))
+			.catch((err) => dispatch({ type: 'dataFailed' }));
+	}, []);
+
 	return (
 		<div className="app">
 			<Header />
