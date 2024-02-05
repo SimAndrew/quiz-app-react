3ed27f4e75894b73b834e980521b773c277a02a2
AndSim <siminiucandrew@gmail.com>
2024-02-05 14:36:27 +0200

    feat: display (progress.jsx) bar with question(numuestions) number and score(points) after answer

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,6 +6,7 @@ import Error from './Error.jsx';
 import StartScreen from './StartScreen.jsx';
 import Question from './Question.jsx';
 import NextButton from './NextButton.jsx';
+import Progress from './Progress.jsx';
 import { useEffect, useReducer } from 'react';
 
 const initialState = {
@@ -54,12 +55,17 @@ function reducer(state, action) {
 }
 
 export default function App() {
-	const [{ questions, status, index, answer }, dispatch] = useReducer(
+	const [{ questions, status, index, answer, points }, dispatch] = useReducer(
 		reducer,
 		initialState,
 	);
 
 	const numQuestions = questions.length;
+
+	const maxPossiblePoints = questions.reduce(
+		(prev, cur) => prev + cur.points,
+		0,
+	);
 
 	useEffect(function () {
 		fetch('http://localhost:8000/questions')
@@ -80,6 +86,14 @@ export default function App() {
 				)}
 				{status === 'active' && (
 					<>
+						<Progress
+							index={index}
+							numQuestions={numQuestions}
+							points={points}
+							maxPossiblePoints={maxPossiblePoints}
+							answer={answer}
+						/>
+
 						<Question
 							question={questions[index]}
 							dispatch={dispatch}
