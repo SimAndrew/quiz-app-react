cf75754da13427931f8fbd14f519b0b1e9baa5c8
AndSim <siminiucandrew@gmail.com>
2024-02-06 21:33:22 +0200

    feat: deploy (questions.json)`www.github.com/typicode/json-server` on `https://my-json-server.typicode.com/`

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -104,7 +104,9 @@ export default function App() {
 	);
 
 	useEffect(function () {
-		fetch('http://localhost:8000/questions')
+		fetch(
+			'https://my-json-server.typicode.com/SimAndrew/quiz-app-react/questions',
+		)
 			.then((res) => res.json())
 			.then((data) => dispatch({ type: 'dataReceived', payload: data }))
 			.catch((err) => dispatch({ type: 'dataFailed' }));
